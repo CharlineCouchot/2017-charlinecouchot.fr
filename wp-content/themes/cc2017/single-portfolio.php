a50970d9fd3be1091e30e69530713daf7699bc6c
Charline Couchot <charline@couchot.fr>
2017-09-16 17:49:33 +0200

    Modifications du portfolio pour les photos

--- wp-content/themes/cc2017/single-portfolio.php
+++ wp-content/themes/cc2017/single-portfolio.php
@@ -6,15 +6,31 @@
 get_header(); ?>
   <div class="block-content" id="body-content">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-      <?php
-       $option_implication = get_field_object('portfolio-implication');
-       $option_client  = get_field_object('portfolio-client');
-       $option_date  = get_field_object('portfolio-date');
-       $option_skills  = get_field_object('portfolio-skills');
-       $option_link  = get_field_object('portfolio-link');
-       $option_gallery  = get_field('portfolio-gallery'); ?>
-          <div class="project-head">
-            <h1 class="block-title"><?php the_title(); ?></h1>
+      <?php $option_implication = get_field_object('portfolio-implication');
+            $option_client  = get_field_object('portfolio-client');
+            $option_date  = get_field_object('portfolio-date');
+            $option_skills  = get_field_object('portfolio-skills');
+            $option_link  = get_field_object('portfolio-link');
+            $option_gallery  = get_field('portfolio-gallery');
+            $cat = get_terms([
+              'taxonomy' => 'portfolio_type'
+            ]);
+            $category = $cat[1]->term_taxonomy_id;?>
+        <?php if($option_gallery && $category == 19) { ?>
+          <div class="project-media row isotope">
+            <?php foreach( $option_gallery as $image ): ?>
+              <div class="col-md-4 col-sm-6 col-xs-12 portfolio-item">
+                <a href="<?php echo $image['url']; ?>" data-fancybox="group" data-caption="<?php echo $image['title']; ?>">
+                  <div class="portfolio-column">
+                    <img src="<?php echo $image['sizes']['medium_large']; ?>" alt="<?php echo $image['alt']; ?>">
+                  </div>
+                </a>
+              </div>
+            <?php endforeach; ?>
+          </div>
+        <?php } ?>
+        <div class="project-head">
+          <h1 class="block-title"><?php the_title(); ?></h1>
             <?php if($option_client['value']) { ?>
               <div class="tags"><h2><?php echo __('Client', 'cc2017'); ?> :</h2> <?php echo $option_client['value']; ?></div>
             <?php } ?>
@@ -28,50 +44,52 @@ get_header(); ?>
                   <span class="tag"><?php print $value->name; ?></span>
                 <?php } ?>
               </div>
+            <?php if($option_place['value']) { ?>
+              <div class="tags"><h2><?php echo __('Lieu', 'cc2017'); ?> :</h2> <?php echo $option_date['value']; ?></div>
             <?php } ?>
-          </div>
-          <div class="project-body">
-            <h2><?php echo __('Quelques mots sur le projet', 'cc2017'); ?> :</h2>
-            <?php the_content(); ?>
-            <?php if($option_implication['value']) { ?>
-              <div class="tags"><h2><?php echo __('Mon implication', 'cc2017'); ?> :</h2> <?php echo $option_implication['value']; ?></div>
-            <?php } ?>
-          </div>
-          <?php if($option_gallery) { ?>
-            <div class="project-media row isotope">
-              <?php foreach( $option_gallery as $image ): ?>
-                <div class="col-md-4 col-sm-6 col-xs-12 portfolio-item">
-                  <a href="<?php echo $image['url']; ?>" data-fancybox="group" data-caption="<?php echo $image['title']; ?>">
-                    <div class="portfolio-column">
-                      <img src="<?php echo $image['sizes']['medium_large']; ?>" alt="<?php echo $image['alt']; ?>">
-                    </div>
-                  </a>
-                </div>
-              <?php endforeach; ?>
-            </div>
+        </div>
+        <div class="project-body">
+          <h2><?php echo __('Quelques mots sur le projet', 'cc2017'); ?> :</h2>
+          <?php the_content(); ?>
+          <?php if($option_implication['value']) { ?>
+            <div class="tags"><h2><?php echo __('Mon implication', 'cc2017'); ?> :</h2> <?php echo $option_implication['value']; ?></div>
           <?php } ?>
-          <div class="project-nav text-center">
-            <div>
-              <?php previous_post_link( '&leftarrow; %link', __('Projet précédent', 'cc2017')); ?>
-            </div>
-            <div>
-              <a id="close-project" href="#"><i class="ion-grid"></i></a>
-            </div>
-            <div>
-              <?php next_post_link( '%link &rightarrow;', __('Projet suivant', 'cc2017')); ?>
-            </div>
+        </div>
+        <?php if($option_gallery && $category == 3) { ?>
+          <div class="project-media row isotope">
+            <?php foreach( $option_gallery as $image ): ?>
+              <div class="col-md-4 col-sm-6 col-xs-12 portfolio-item">
+                <a href="<?php echo $image['url']; ?>" data-fancybox="group" data-caption="<?php echo $image['title']; ?>">
+                  <div class="portfolio-column">
+                    <img src="<?php echo $image['sizes']['medium_large']; ?>" alt="<?php echo $image['alt']; ?>">
+                  </div>
+                </a>
+              </div>
+            <?php endforeach; ?>
           </div>
-          <script>
-            jQuery(window).on('load', function() {
-              var $container = jQuery('.project-media');
-              $container.isotope({
-                masonry: {
-                  columnWidth: '.portfolio-item'
-                },
-                itemSelector: '.portfolio-item'
-              });
+        <?php } ?>
+        <div class="project-nav text-center">
+          <div>
+            <?php previous_post_link( '&leftarrow; %link', __('Projet précédent', 'cc2017')); ?>
+          </div>
+          <div>
+            <a id="close-project" href="#"><i class="ion-grid"></i></a>
+          </div>
+          <div>
+            <?php next_post_link( '%link &rightarrow;', __('Projet suivant', 'cc2017')); ?>
+          </div>
+        </div>
+        <script>
+          jQuery(window).on('load', function() {
+            var $container = jQuery('.project-media');
+            $container.isotope({
+              masonry: {
+                columnWidth: '.portfolio-item'
+              },
+              itemSelector: '.portfolio-item'
             });
-          </script>
+          });
+        </script>
     <?php endwhile; endif; ?>
   </div>
 
